21ed99eb8c6a77aa9cb9ffafb0e6c22af09df631
jonascarvalh <olaffic@gmail.com>
2023-04-26 19:19:52 -0300

    solving bug category view and create test files

--- recipes/views.py
+++ recipes/views.py
@@ -24,7 +24,7 @@ def category(request, category_id):
 
     return render(request, 'recipes/pages/category.html', context={
         'recipes': recipes,
-        'title': f'{recipes.first().category.name} - Category'
+        'title': f'{recipes[0].category.name} - Category'
     })
 
 def recipe(request, id):
