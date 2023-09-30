1ea237b50f99bd8b5396dfc63c6683b1aeb139ad
jonascarvalh <olaffic@gmail.com>
2023-09-30 17:03:33 -0300

    create a environment for recipes in page (PER_PAGE)

--- recipes/views.py
+++ recipes/views.py
@@ -5,6 +5,9 @@ from django.db.models import Q
 from utils.pagination import make_pagination
 # HTTP REQUEST <- HTTP RESPONSE
 # HTTP Request
+import os 
+
+PER_PAGE = os.environ.get('PER_PAGE', 9)
 
 # Create your views here.
 def home(request):
@@ -12,7 +15,7 @@ def home(request):
         is_published=True
     ).order_by('-id')
 
-    page_obj, pagination_range = make_pagination(request, recipes, 9)
+    page_obj, pagination_range = make_pagination(request, recipes, PER_PAGE)
 
     return render(request, 'recipes/pages/home.html', context={
         'recipes': page_obj,
@@ -27,7 +30,7 @@ def category(request, category_id):
         ).order_by('-id')
     )
 
-    page_obj, pagination_range = make_pagination(request, recipes, 9)
+    page_obj, pagination_range = make_pagination(request, recipes, PER_PAGE)
 
     return render(request, 'recipes/pages/category.html', context={
         'recipes': page_obj,
@@ -60,7 +63,7 @@ def search(request):
         is_published=True
     ).order_by('-id')
     
-    page_obj, pagination_range = make_pagination(request, recipes, 9)
+    page_obj, pagination_range = make_pagination(request, recipes, PER_PAGE)
     return render(request, 'recipes/pages/search.html',{
         'page_title': f'Search for "{search_term}"',
         'search_term': search_term,
