7f65e761cffc76167d7df85d7cce368d4f4552cf
jonascarvalh <olaffic@gmail.com>
2023-10-16 23:52:38 -0300

    add csrf token in form and create condition in register_view

--- authors/views.py
+++ authors/views.py
@@ -3,7 +3,11 @@ from .forms import RegisterForm
 
 # Create your views here.
 def register_view(request):
-    form = RegisterForm()
+    if request.POST:
+        form = RegisterForm(request.POST)
+    else:
+        form = RegisterForm()
+        
     return render(
         request, 
         'authors/pages/register_view.html',
