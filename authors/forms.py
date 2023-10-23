a179640f4619f5fa9542746c191771ddb47f5ed1
jonascarvalh <olaffic@gmail.com>
2023-10-23 19:04:04 -0300

    re-use add_attr with add_placeholder

--- authors/forms.py
+++ authors/forms.py
@@ -6,7 +6,7 @@ def add_attr(field, attr_name, attr_new_val):
     field.widget.attrs[attr_name] = f'{existing_attr} {attr_new_val}'.strip()
 
 def add_placeholder(field, placeholder_val):
-    field.widget.attrs['placeholder'] = placeholder_val
+    add_attr(field, 'placeholder', placeholder_val)
 
 class RegisterForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
@@ -15,7 +15,7 @@ class RegisterForm(forms.ModelForm):
         add_placeholder(self.fields['email'], 'Your e-mail')
         add_placeholder(self.fields['first_name'], 'Your first name')
         add_placeholder(self.fields['last_name'], 'Your last name')
-
+        add_attr(self.fields['username'], 'css', 'a-css-class')
 
 
     password = forms.CharField(
