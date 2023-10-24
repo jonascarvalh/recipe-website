29c80ddd5bea779025c30475177cb973d52dae51
jonascarvalh <olaffic@gmail.com>
2023-10-24 19:57:23 -0300

    using validators atribute of field for validation (strong_password)

--- authors/forms.py
+++ authors/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
+import re
 
 def add_attr(field, attr_name, attr_new_val):
     existing_attr = field.widget.attrs.get(attr_name, '')
@@ -8,6 +9,19 @@ def add_attr(field, attr_name, attr_new_val):
 
 def add_placeholder(field, placeholder_val):
     add_attr(field, 'placeholder', placeholder_val)
+
+def strong_password(password):
+    regex = re.compile(r'^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).{8,}$')
+
+    if not regex.match(password):
+        raise ValidationError(
+            (
+                'Password must have at least one uppercase letter, '
+                'one lower case letter and one number. The length should be '
+                'at least 8 characters.'
+            ),
+            code='invalid'
+        )
 
 class RegisterForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
@@ -30,7 +44,8 @@ class RegisterForm(forms.ModelForm):
             'Password must have at least one uppercase letter, '
             'one lower case letter and one number. The length should be '
             'at least 8 characters.'
-        )
+        ),
+        validators=[strong_password]
     )
 
     password2 = forms.CharField(
@@ -112,6 +127,9 @@ class RegisterForm(forms.ModelForm):
         cleaned_data = super().clean()
         password = cleaned_data.get('password')
         password2 = cleaned_data.get('password2')
+        
+        if password is None:
+            return
         
         if password != password2:
             password_confirmation_error = ValidationError(
