30f71b338f731cd8493a14be2e235f11123f2dff
jonascarvalh <olaffic@gmail.com>
2023-11-03 19:11:48 -0300

    add integration test for empty field in register form

--- authors/tests/test_author_register_form.py
+++ authors/tests/test_author_register_form.py
@@ -1,6 +1,9 @@
+from typing import Any
 from authors.forms import RegisterForm
 from parameterized import parameterized
 from unittest import TestCase
+from django.test import TestCase as DjangoTestCase
+from django.urls import reverse
 
 class AuthorRegisterFormUnitTest(TestCase):
     @parameterized.expand([
@@ -45,3 +48,24 @@ class AuthorRegisterFormUnitTest(TestCase):
         current = form[field].field.label
 
         self.assertEqual(current, needed)
+
+class AuthorRegisterFormIntegrationTest(DjangoTestCase):
+    def setUp(self, *args, **kwargs):
+        self.form_data = {
+            'username': 'user',
+            'first_name': 'first',
+            'last_name': 'last',
+            'email': 'email@anyemail.com',
+            'password': 'Str0ngP@ssword1',
+            'password2': 'Str0ngP@ssword1',
+        }
+        return super().settings(*args, **kwargs)
+    
+    @parameterized.expand([
+        ('username', 'This field must not be empty')
+    ])
+    def test_fields_cannot_be_empty(self, field, msg):
+        self.form_data[field] = ''
+        url = reverse('authors:create')
+        response = self.client.post(url, data=self.form_data, follow=True)
+        self.assertIn(msg, response.content.decode('utf-8'))
