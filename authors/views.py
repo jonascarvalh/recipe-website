2319e2b192fd59cc453e9c5fb8e2af700e67fbff
jonascarvalh <olaffic@gmail.com>
2023-11-05 17:33:35 -0300

    organizing files

--- authors/views.py
+++ authors/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from .forms import RegisterForm
 from django.http import Http404
 from django.contrib import messages
-
+from django.urls import reverse
 # Create your views here.
 def register_view(request):
     register_form_data = request.session.get('register_form_data', None)
@@ -10,9 +10,9 @@ def register_view(request):
     form = RegisterForm(register_form_data)  
     return render(
         request, 
-        'authors/pages/register_view.html',
-        {
-            'form': form
+        'authors/pages/register_view.html', {
+            'form': form,
+            'form_action': reverse('authors:create'),
         }
     )
 
