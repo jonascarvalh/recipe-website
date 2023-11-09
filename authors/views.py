ecee1239dd15475ce938d37b0274f2840153cb18
jonascarvalh <olaffic@gmail.com>
2023-11-09 17:49:14 -0300

    add login handling

--- authors/views.py
+++ authors/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.http import Http404
 from django.contrib import messages
 from django.urls import reverse
+from django.contrib.auth import authenticate, login
 
 from .forms import RegisterForm, LoginForm
 
@@ -44,4 +45,24 @@ def login_view(request):
     })
 
 def login_create(request):
-    return render(request, 'authors/pages/login.html')
+    if not request.POST:
+        raise Http404()
+    
+    form = LoginForm(request.POST)
+    login_url = reverse('authors:login')
+
+    if form.is_valid():
+        authenticated_user = authenticate(
+            username=form.cleaned_data.get('username', ''),
+            password=form.cleaned_data.get('password', '')
+        )
+    
+        if authenticated_user is not None:
+            messages.success(request, 'You are logged in.')
+            login(request, authenticated_user)
+        else:
+            messages.error(request, 'Invalid credentials')
+    else:
+        messages.error(request, 'Invalid username or password')
+    
+    return redirect(login_url)
