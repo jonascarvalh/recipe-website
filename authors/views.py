1da564a393469313242c6f474a3c77ebece2230b
jonascarvalh <olaffic@gmail.com>
2023-11-09 16:52:36 -0300

    add login_form and mdified view for render new login

--- authors/views.py
+++ authors/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render, redirect
-from .forms import RegisterForm
 from django.http import Http404
 from django.contrib import messages
 from django.urls import reverse
-# Create your views here.
+
+from .forms import RegisterForm, LoginForm
+
 def register_view(request):
     register_form_data = request.session.get('register_form_data', None)
 
@@ -36,7 +37,11 @@ def register_create(request):
     return redirect('authors:register')
 
 def login_view(request):
-    return render(request, 'authors/pages/login.html')
+    form = LoginForm()
+    return render(request, 'authors/pages/login.html', {
+        'form': form,
+        'form_action': reverse('authors:login_create')
+    })
 
 def login_create(request):
     return render(request, 'authors/pages/login.html')
