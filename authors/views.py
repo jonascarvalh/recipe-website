81affdabdd77408e683f6498ae59a897fe282445
jonascarvalh <olaffic@gmail.com>
2023-11-10 15:28:45 -0300

    add logout system and security rules

--- authors/views.py
+++ authors/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render, redirect
 from django.http import Http404
 from django.contrib import messages
 from django.urls import reverse
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 
 from .forms import RegisterForm, LoginForm
 
@@ -34,7 +35,8 @@ def register_create(request):
         messages.success(request, 'Your user is created, please log in.')
 
         del(request.session['register_form_data'])
-        
+        return redirect(reverse('authors:login'))
+    
     return redirect('authors:register')
 
 def login_view(request):
@@ -56,7 +58,7 @@ def login_create(request):
             username=form.cleaned_data.get('username', ''),
             password=form.cleaned_data.get('password', '')
         )
-    
+
         if authenticated_user is not None:
             messages.success(request, 'You are logged in.')
             login(request, authenticated_user)
@@ -66,3 +68,14 @@ def login_create(request):
         messages.error(request, 'Invalid username or password')
     
     return redirect(login_url)
+
+@login_required(login_url='authors:login', redirect_field_name='next')
+def logout_view(request):
+    if not request.POST:
+        return redirect(reverse('authors:login'))
+    
+    if request.POST.get('username') != request.user.username:
+        return redirect(reverse('authors:login'))
+    
+    logout(request)
+    return redirect(reverse('authors:login'))
