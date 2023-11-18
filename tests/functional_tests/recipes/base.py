f3b6d1b07e0314b0dce5b72bf3020335159c403a
jonascarvalh <olaffic@gmail.com>
2023-11-18 15:49:14 -0300

    organize functional tests

--- tests/functional_tests/recipes/base.py
+++ tests/functional_tests/recipes/base.py
@@ -2,9 +2,7 @@ from django.test import LiveServerTestCase
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 
 from utils.browser import make_chrome_browser
-from selenium.webdriver.common.by import By
 import time
-
 class RecipeBaseFunctionalTest(StaticLiveServerTestCase):
     def setUp(self) -> None:
         self.browser = make_chrome_browser()
@@ -16,9 +14,3 @@ class RecipeBaseFunctionalTest(StaticLiveServerTestCase):
     
     def sleep(self, seconds=5):
         time.sleep(seconds)
-
-class RecipeHomepageFunctionalTest(RecipeBaseFunctionalTest):
-    def test_recipe_home_page_without_recipes_not_found_message(self):
-        self.browser.get(self.live_server_url)
-        body = self.browser.find_element(By.TAG_NAME, 'body')
-        self.assertIn('No recipes found here 😭', body.text)
---
+++ tests/functional_tests/recipes/test_recipe_homepage.py
@@ -0,0 +1,8 @@
+from selenium.webdriver.common.by import By
+from .base import RecipeBaseFunctionalTest
+
+class RecipeHomepageFunctionalTest(RecipeBaseFunctionalTest):
+    def test_recipe_home_page_without_recipes_not_found_message(self):
+        self.browser.get(self.live_server_url)
+        body = self.browser.find_element(By.TAG_NAME, 'body')
+        self.assertIn('No recipes found here 😭', body.text)
