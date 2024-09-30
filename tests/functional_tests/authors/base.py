c3a2e96a7f424d440c01ec7847a0dacd5265927a
jonascarvalh <olaffic@gmail.com>
2024-09-29 22:01:19 -0300

    chore: setup functional test to authors register

---
+++ tests/functional_tests/authors/base.py
@@ -0,0 +1,15 @@
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from utils.browser import make_chrome_browser
+import time
+
+class AuthorsBaseTest(StaticLiveServerTestCase):
+    def setUp(self) -> None:
+        self.browser = make_chrome_browser()
+        return super().setUp()
+    
+    def tearDown(self) -> None:
+        self.browser.quit()
+        return super().tearDown()
+    
+    def sleep(self, qtd=3):
+        time.sleep(qtd)
