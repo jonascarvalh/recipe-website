ae2229c9a4121fe05062d21ca3380f8b298cc75a
jonascarvalh <olaffic@gmail.com>
2024-10-04 17:15:15 -0300

    chopre: update coveragerc and add test to user try get request in redirect login

--- tests/functional_tests/authors/test_authors_login.py
+++ tests/functional_tests/authors/test_authors_login.py
@@ -34,3 +34,10 @@ class AuthorsLoginTest(AuthorsBaseTest):
             f"You are logged in with {user.username}.",
             self.browser.find_element(By.TAG_NAME, "body").text
         )
+    
+    def test_login_create_raises_404_if_not_post_method(self):
+        self.browser.get(self.live_server_url + reverse("authors:login_create"))
+        self.assertIn(
+            "Not Found",
+            self.browser.find_element(By.TAG_NAME, "body").text
+        )
