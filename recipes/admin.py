51da80def65283ca1130b1a31d064e5081d092fa
jonascarvalh <olaffic@gmail.com>
2024-10-06 21:55:07 -0300

    chore: add customization for recipes in django admin

--- recipes/admin.py
+++ recipes/admin.py
@@ -9,7 +9,12 @@ class CategoryAdmin(admin.ModelAdmin):
 
 @admin.register(Recipe)
 class RecipeAdmin(admin.ModelAdmin):
-    ...
+    list_display = ["id", "title", "created_at", "is_published"]
+    list_display_links = ["title", "created_at"]
+    search_fields = ["id", "title", "description", "slug", "preparation_steps"]
+    list_filter = ["category", "author", "is_published", "preparation_steps_is_html"]
+    list_per_page = 10
+    list_editable = ["is_published", ]
+    ordering = ["-id", ]
 
 admin.site.register(Category, CategoryAdmin)
-
