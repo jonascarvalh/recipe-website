4dce276e2204a13621ab0d163afb429af55ffecb
jonascarvalh <olaffic@gmail.com>
2024-11-11 23:14:04 -0300

    chore: add POST method and refactor CBV

--- authors/views/dashboard_recipe.py
+++ authors/views/dashboard_recipe.py
@@ -7,15 +7,36 @@ from django.shortcuts import redirect, render
 from django.urls import reverse
 
 class DashboardRecipe(View):
-    def get(self, request, id):
-        recipe = Recipe.objects.filter(
-            is_published=False,
-            author=request.user,
-            pk=id
-        ).first()
+    def get_recipe(self, id):
+        recipe = None
 
-        if not recipe:
-            raise Http404()
+        if id:
+            recipe = Recipe.objects.filter(
+                is_published=False,
+                author=self.request.user,
+                pk=id
+            ).first()
+
+            if not recipe:
+                raise Http404()
+        
+        return recipe
+
+    def render_recipe(self, form):
+        return render(
+            self.request, 
+            "authors/pages/dashboard_recipe.html", {
+                "form": form
+            }
+        )
+    
+    def get(self, request, id):
+        recipe = self.get_recipe(id)
+        form = AuthorRecipeForm(instance=recipe)
+        return self.render_recipe(form)
+
+    def post(self, request, id):
+        recipe = self.get_recipe(id)
         
         form = AuthorRecipeForm(
             data=request.POST or None,
@@ -36,10 +57,4 @@ class DashboardRecipe(View):
             messages.success(request, "Sua receita foi salva com sucesso.")
             return redirect(reverse("authors:dashboard_recipe_edit", args=(id,)))
         
-        return render(
-            request, 
-            "authors/pages/dashboard_recipe.html",
-            {
-                "form": form
-            }
-        )
+        return self.render_recipe(form)
