8ddfe1d8aa706e48129c46804e2e589c275e0620
jonascarvalh <olaffic@gmail.com>
2024-11-16 16:33:56 -0300

    chore: create Home, Category and Search View (CBV)

--- recipes/views.py
+++ recipes/views.py
@@ -35,8 +35,56 @@ class RecipeListViewBase(ListView):
         )
         ctx.update({
             'recipes': page_obj,
-            'paginantion_range': pagination_range
+            'pagination_range': pagination_range
         })
+        return ctx
+
+class RecipeListViewHome(RecipeListViewBase):
+    template_name = 'recipes/pages/home.html'
+
+class RecipeListViewCategory(RecipeListViewBase):
+    template_name = 'recipes/pages/category.html'
+
+    def get_queryset(self, *args, **kwargs):
+        qs = super().get_queryset(*args, **kwargs)
+        qs = qs.filter(
+            category__id=self.kwargs.get('category_id'),
+            is_published=True
+        )
+        return qs
+    
+    def get_context_data(self, *args, **kwargs):
+        ctx = super().get_context_data(*args, **kwargs)
+        ctx.update({
+            'title': f'{ctx.get('recipes')[0].category.name} - Category'
+        })
+        return ctx
+
+class RecipeListViewSearch(RecipeListViewBase):
+    template_name = 'recipes/pages/search.html'
+    
+    def get_queryset(self, *args, **kwargs):
+        self.search_term = self.request.GET.get('q', '').strip()
+
+        qs = super().get_queryset(*args, **kwargs)
+        qs = qs.filter(
+            Q(
+                Q(title__icontains=self.search_term) | # "OR" in database
+                Q(description__icontains=self.search_term),
+            ),
+            is_published=True
+        )
+
+        return qs
+
+    def get_context_data(self, *args, **kwargs):
+        ctx = super().get_context_data(*args, **kwargs)        
+        ctx.update({
+            'search_term': self.search_term,
+            'page_title': f'Search for "{self.search_term}"',
+            'additional_url_query': f'&q={self.search_term}'
+        })
+        return ctx
 
 def home(request):
     recipes = Recipe.objects.filter(
