92c870f58ed34e37950a3f9217593a0a6dc5f592
jonascarvalh <olaffic@gmail.com>
2024-11-16 15:39:37 -0300

    chore: create base class for home, category and search

--- recipes/views.py
+++ recipes/views.py
@@ -1,17 +1,43 @@
+from django.db.models.query import QuerySet
 from django.shortcuts import render, get_list_or_404, get_object_or_404
 from django.http import HttpResponse, Http404
 from recipes.models import Recipe
 from django.db.models import Q
 from utils.pagination import make_pagination
 from django.contrib import messages
-
+from django.views.generic import ListView
 # HTTP REQUEST <- HTTP RESPONSE
 # HTTP Request
 import os 
 
 PER_PAGE = int(os.environ.get('PER_PAGE', 9))
 
-# Create your views here.
+class RecipeListViewBase(ListView):
+    model = Recipe
+    paginate_by = None
+    context_object_name = 'recipes'
+    ordering = ['-id']
+    template_name = 'recipes/pages/home.html'
+
+    def get_queryset(self, *args, **kwargs):
+        qs = super().get_queryset(*args, **kwargs)
+        qs = qs.filter(
+            is_published=True
+        )
+        return qs
+    
+    def get_context_data(self, *args, **kwargs):
+        ctx = super().get_context_data(*args, **kwargs)
+        page_obj, pagination_range = make_pagination(
+            self.request, 
+            ctx.get('recipes'), 
+            PER_PAGE
+        )
+        ctx.update({
+            'recipes': page_obj,
+            'paginantion_range': pagination_range
+        })
+
 def home(request):
     recipes = Recipe.objects.filter(
         is_published=True
